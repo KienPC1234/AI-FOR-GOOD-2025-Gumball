6c1b9287bbc3b9d54589c823bf02f54cc1628caa
KienTF_VSCODE <kienp@KIEN-PC>
2025-04-22 19:47:52 +0700

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AI_FOR_GOOD_2025___Gumball.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
